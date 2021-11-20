f555084d8281428fc2644547ae955a5bd98af4cb
Pat T <pted0001@student.monash.edu>
2021-11-20 22:29:12 +1100

    location js

---
+++ locate.js
@@ -0,0 +1,33 @@
+
+
+// Nearby Search How to do//
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDo2D2IF2jBJQ7nH62Sm4AyidwR5SZy9S0',
+  headers: { }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+//Use Geolocation to detect where someone is//
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
