2054344ffbddce300dec1a099f27f9c8c4640916
lionie_wijaya <lwij0012@student.monash.edu>
2021-11-21 16:07:17 +0800

    change to external css

--- camera.js
+++ camera.js
@@ -1,108 +1,17 @@
-const vision = require(["@google-cloud/vision"], function (require) {
-    var namedModule = require("@google-cloud/vision");
-});
-
-// Creates a client
-const client = new vision.ImageAnnotatorClient({
-    keyFilename: "visionAPIKey.json",
-});
-
-// Ini camera related, can ignore, intinya utk set output 
-(function () {
-    var width = 320; // We will scale the photo width to this
-    var height = 0; // This will be computed based on the input stream
-
-    var streaming = false;
-
-    var video = null;
-    var canvas = null;
-    var photo = null;
-    var startbutton = null;
-
-    function startup() {
-        video = document.getElementById("video");
-        canvas = document.getElementById("canvas");
-        photo = document.getElementById("photo");
-        startbutton = document.getElementById("startbutton");
-
-        navigator.mediaDevices
-            .getUserMedia({
-                video: true,
-                audio: false,
-            })
-            .then(function (stream) {
-                video.srcObject = stream;
-                video.play();
-            })
-            .catch(function (err) {
-                console.log("An error occurred: " + err);
-            });
-
-        video.addEventListener(
-            "canplay",
-            function (ev) {
-                if (!streaming) {
-                    height = video.videoHeight / (video.videoWidth / width);
-
-                    if (isNaN(height)) {
-                        height = width / (4 / 3);
-                    }
-
-                    video.setAttribute("width", width);
-                    video.setAttribute("height", height);
-                    canvas.setAttribute("width", width);
-                    canvas.setAttribute("height", height);
-                    streaming = true;
-                }
-            },
-            false
-        );
-
-        startbutton.addEventListener(
-            "click",
-            function (ev) {
-                takepicture();
-                ev.preventDefault();
-            },
-            false
-        );
-
-        clearphoto();
-    }
-
-    function clearphoto() {
-        var context = canvas.getContext("2d");
-        context.fillStyle = "#AAA";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        var data = canvas.toDataURL("image/png");
-        photo.setAttribute("src", data);
-    }
-
-    function takepicture() {
-        var context = canvas.getContext("2d");
-        if (width && height) {
-            canvas.width = width;
-            canvas.height = height;
-            context.drawImage(video, 0, 0, width, height);
-
-            var data = canvas.toDataURL("image/png");
-            photo.setAttribute("src", data);
-        } else {
-            clearphoto();
-        }
-    }
-
-    window.addEventListener("load", startup, false);
-})();
-
 // Detect function, output json from photo
 function detect() {
-    photo = document.getElementById("photo");
+    const vision = require("@google-cloud/vision");
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient({
+        keyFilename: "visionAPIKey.json",
+    });
+
+    // photo = document.getElementById("photo");
 
     // Performs label detection on the image file
     client
-        .labelDetection(photo) //temporary img, use img from id=photo
+        .labelDetection("./Images/apple.jpg") //temporary img, use img from id=photo "./Images/apple.jpg"
         .then((results) => {
             const labels = results[0].labelAnnotations;
 
@@ -114,3 +23,5 @@ function detect() {
             console.error("ERROR:", err);
         });
 }
+
+// detect("./Images/apple.jpg");
